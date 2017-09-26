851dea18763f8707e1aa45be08084e55bf550872
Sejun Im <sejun425@gmail.com>
2017-09-26 12:13:35 +0900

    post_detail 페이지 작성

--- djangogirls/blog/views.py
+++ djangogirls/blog/views.py
@@ -10,3 +10,12 @@ def post_list(request):
     }
     return render(request, 'blog/post_list.html', context)
 
+#post_detail 기능 함수 구현
+#'post'라는 key로 Post.objects.first()에 해당하는 객체를 전달
+# 템플릿은 blog/post_detail.html을 사용
+def post_detail(request):
+    context = {
+        'post': Post.objects.first()
+    }
+    return render(request, 'blog/post_detail.html', context)
+
